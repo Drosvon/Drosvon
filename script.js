4632c9269b25c9084bab56e4d23fcb5097e26a38
Drosvon <drozvon@gmail.com>
2024-08-20 16:00:15 +0700

    PortoDescTest4

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("cards").onmousemove = e => {
     const rect = card.getBoundingClientRect(),
           x = e.clientX - rect.left,
           y = e.clientY - rect.top;
-
+ 
     card.style.setProperty("--mouse-x", `${x}px`);
     card.style.setProperty("--mouse-y", `${y}px`);
   };
